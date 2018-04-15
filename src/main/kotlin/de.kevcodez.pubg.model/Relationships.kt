145b4b815ea3b54970ac81916a62161eb8a7bb57
Kevin <k.grueneberg1994@gmail.com>
2018-04-15 16:03:22 +0200

    Parse participant and roster ids in Relationships-Entity

--- src/main/kotlin/de.kevcodez.pubg.model/Relationships.kt
+++ src/main/kotlin/de.kevcodez.pubg.model/Relationships.kt
@@ -1,11 +1,37 @@
 package de.kevcodez.pubg.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.ArrayNode
+
 data class Relationships(
     val matches: Matches?,
-    val participants: Any?,
+    var participantIds: List<String>?,
     val team: Any?,
     val assets: Any?,
-    val rosters: Any?,
+    var rosterIds: List<String>?,
     val rounds: Any?,
     val spectators: Any?
+) {
+    @JsonProperty("participants")
+    private fun unpackParticipantIds(participants: JsonNode) {
+        val data = participants.get("data") as ArrayNode
+
+        participantIds = data.map { it.get("id").textValue() }.toList()
+    }
+
+    @JsonProperty("rosters")
+    private fun unpackRosterIds(rosters: JsonNode) {
+        val data = rosters.get("data") as ArrayNode
+
+        rosterIds = data.map { it.get("id").textValue() }.toList()
+    }
+}
+
+data class RelationParticipantData(
+    val data: RelationParticipant
+)
+
+data class RelationParticipant(
+    val id: String
 )
